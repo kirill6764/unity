f3dcd59465ec744d99d2e7f1f7983808dd05540d
kirill6764 <k9131471912@gmail.com>
2017-10-05 22:13:18 +0600

    Обновил

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CameraScript : MonoBehaviour {
+	bool camAvailable;
+	WebCamTexture webcamTexture;
+	Texture defaultBackground;
+	public RawImage rawImage;
+	public AspectRatioFitter fit;
+	// Use this for initialization
+	void Start () {
+		defaultBackground = rawImage.texture;
+		WebCamDevice[] devices = WebCamTexture.devices;
+		if (devices.Length == 0) {
+			Debug.Log ("No camera");
+			camAvailable = false;
+			return;
+		}
+
+		for(int i = 0; i < devices.Length; i++){
+			if(devices[i].isFrontFacing){
+				webcamTexture = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+			}
+		}
+
+		if(webcamTexture == null){
+			Debug.Log("Unable to find camera");
+			return;
+		}
+		webcamTexture.Play();
+		rawImage.texture = webcamTexture;
+		camAvailable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!camAvailable)
+			SceneManager.LoadScene ("Main");
+		else {
+			float ratio = (float)webcamTexture.width / (float)webcamTexture.height;
+			fit.aspectRatio = ratio;
+
+			float scaleY = webcamTexture.videoVerticallyMirrored ? -1f : 1f;
+			rawImage.rectTransform.localScale = new Vector3 (1f, scaleY, 1f);
+
+			int orient = -webcamTexture.videoRotationAngle;
+			rawImage.rectTransform.localEulerAngles = new Vector3 (0, 0, orient);
+		}
+	}
+
+	public void SavePhoto(){
+		string _SavePath = Application.persistentDataPath + "/";
+		Texture2D snap = new Texture2D(rawImage.mainTexture.width, rawImage.mainTexture.height, TextureFormat.ARGB32, false);
+		snap.SetPixels(webcamTexture.GetPixels()); 
+		snap.Apply();
+		snap = PortraitToLandscape(snap);
+		System.IO.File.WriteAllBytes(_SavePath + "avatar.png", snap.EncodeToPNG());
+		SceneManager.LoadScene("Main");
+	}
+
+	Texture2D PortraitToLandscape(Texture2D origTex) {
+		var origPix = origTex.GetPixels();
+		var newPix = new Color[origPix.Length];
+
+		for (var x = 0; x < origTex.width; x++) {
+			for (var y = 0; y < origTex.height; y++) {
+				var newX = y;
+				var newY = origTex.width - x - 1;
+				newPix[y * origTex.width + x] = origPix[newY * origTex.height + newX];
+			}
+		}
+		var newTex = new Texture2D(origTex.height, origTex.width);
+		newTex.SetPixels(newPix);
+		newTex.Apply();
+		return newTex;
+	}
+}
---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,148 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+public class MainScript : MonoBehaviour {
+
+	public  InputField user_name;
+	public  InputField user_secondName;
+	public InputField user_email;
+	public static MainScript instance;
+
+	public Text errorsText;
+
+	private static bool sendMail;
+	private string path;
+
+	void Awake(){
+		if (!instance) {
+			instance = this;
+			DontDestroyOnLoad (transform.gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+	void OnLevelWasLoaded(int level){
+		
+		Debug.Log ("Load Scene");
+	}
+
+    void Start () {
+		sendMail = false;
+		path = Application.persistentDataPath + "/";
+		string pathSCV = path + "save.csv";
+		if (!File.Exists(pathSCV))
+        {
+			StreamWriter csv = new StreamWriter(pathSCV, true, System.Text.Encoding.UTF8);
+            csv.WriteLine("Имя;Фамилия;Почта"); // это заголовок. разделитель ; может быть другим
+            csv.Close();
+        }
+    }
+
+	
+
+	public void MyOnCLickButton(){
+		string pathSCV = path + "save.csv";
+        //string path = Application.persistentDataPath + "/save.csv";
+
+        List<string> errors = new List<string>();
+		errorsText.text = "";
+		if (user_name.text == "") {
+			errors.Add("Не заполнено имя");
+		}
+		if (user_secondName.text == "") {
+			errors.Add("Не заполнена фамилия");
+		}
+		if (user_email.text == "") {
+			errors.Add("Не заполнена почта");
+		}else if(!isValidMail(user_email.text)){
+			errors.Add("Email не корректен");
+		}
+		if (errors.Count > 0) {
+			errorsText.text = errors [0];
+		} else {
+			errorsText.text = "Тестовое задание";
+			StreamWriter csv = new StreamWriter(pathSCV, true, System.Text.Encoding.UTF8);
+			csv.WriteLine(user_name.text + ";" + user_secondName.text + ";" + user_email.text); // это заголовок. разделитель ; может быть другим
+			csv.Close();
+			if (File.Exists (path + "avatar.png")) {
+				ThreadStart threadStart = new ThreadStart(SendMail);
+				Thread thread = new Thread (threadStart);
+				thread.IsBackground = true;
+				thread.Start();
+			} else {
+				errorsText.text = "Необходимо сделать фото";
+				return;
+			}
+		}
+    }
+
+	public void CreatePhoto(){
+		SceneManager.LoadScene ("Camera");
+	}
+
+	private void SendMail()
+	{
+		string pathPhoto = path + "avatar.png";
+		//smtp сервер
+		string smtpHost = "smtp.timeweb.ru";
+		//smtp порт
+		int smtpPort = 2525;
+		//логин
+		string login = "kirill@klibr.ru";
+		//пароль
+		string pass = "XM75k9nE";
+		 
+		//создаем подключение
+		SmtpClient client = new SmtpClient(smtpHost, smtpPort);
+
+		client.Credentials = ( (System.Net.ICredentialsByHost) (new NetworkCredential(login, pass)));
+
+		//От кого письмо
+		string from = "kirill@klibr.ru";
+		//Кому письмо
+		string to = user_email.text;
+		//Тема письма
+		string subject = "Тестовое приложение. Письмо";
+		//Текст письма
+		string body = "Привет, " + user_name.text + "! \n\n Ваше фото.";
+
+		//Создаем сообщение
+		MailMessage mess = new MailMessage(from, to, subject, body);
+		Attachment attach = new Attachment (pathPhoto);
+		mess.Attachments.Add (attach);
+			
+		try{
+			client.Send(mess);
+			sendMail = true;
+		}catch(System.Exception ex){
+		}
+	}
+	public bool isValidMail(string email)
+	{
+		string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+		Match isMatch = Regex.Match(email, pattern, RegexOptions.IgnoreCase);
+		return isMatch.Success;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (sendMail) {
+			errorsText.text = "Сообщение отправлено";
+		}
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if (Input.GetKeyDown(KeyCode.Escape)) 
+			{
+				Application.Quit(); 
+			}
+		}
+	}
+}
